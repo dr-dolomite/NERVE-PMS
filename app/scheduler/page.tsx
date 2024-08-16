0a127ec809e8ccaef5f4c4511aa6de1da7f455c7
Pio Lawrence Burgos <piolawrence.burgos@wvsu.edu.ph>
2024-08-16 16:58:39 +0800

    3rd Commit:Modified the input forms with Custom DateTime Picker

--- app/scheduler/page.tsx
+++ app/scheduler/page.tsx
@@ -7,83 +7,128 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin, { DropArg } from "@fullcalendar/interaction";
 import googleCalendarPlugin from "@fullcalendar/google-calendar";
 
+// import { Calendar as CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { toast } from "@/components/ui/use-toast";
+
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { TimePickerDemo } from "./time-picker-demo"; // Make sure this import is correct
+import { FaCalendar } from "react-icons/fa";
+
+const FormSchema = z.object({
+  eventDateTime: z.date({
+    required_error: "An event date and time is required.",
+  }),
+  title: z.string().min(1, "Event title is required"),
+  patientName: z.string().min(1, "Patient name is required"),
+  description: z.string().optional(),
+});
 
 interface Event {
   title: string;
   start: Date | string;
   allDay: boolean;
   id: number;
+  patientName?: string;
+  description?: string;
 }
 
 export default function Scheduler() {
-  const [allEvents, setAllEvents] = useState<Event[]>([]); // State to store all events
-  const [showModal, setShowModal] = useState(false); // State to control the visibility of the dialog
-  const [newEvent, setNewEvent] = useState<Event>({
-    title: "",
-    start: "",
-    allDay: false,
-    id: 0,
-  }); // State to store the new event being created
+  const [allEvents, setAllEvents] = useState<Event[]>([]);
+  const [showModal, setShowModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [idToDelete, setIdToDelete] = useState<number | null>(null);
 
-  // Function to handle the date click in the calendar
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      eventDateTime: new Date(),
+      title: "",
+      patientName: "",
+      description: "",
+    },
+  });
+
   function handleDateClick(arg: { date: Date; allDay: boolean }) {
-    setNewEvent({
-      ...newEvent,
-      start: arg.date, // Set the start date of the new event
-      allDay: arg.allDay, // Set whether the event is all-day
-      id: new Date().getTime(), // Generate a unique ID for the new event
-    });
-    setShowModal(true); // Show the dialog for event creation
+    form.setValue("eventDateTime", arg.date);
+    setShowModal(true);
   }
 
-  // Function to add a new event to the calendar
   function addEvent(data: DropArg) {
-    const event = {
-      ...newEvent,
-      start: data.date.toISOString(),
+    const newEvent: Event = {
       title: data.draggedEl.innerText,
+      start: data.date,
       allDay: data.allDay,
       id: new Date().getTime(),
     };
-    setAllEvents([...allEvents, event]);
+    setAllEvents([...allEvents, newEvent]);
+  }
+
+  function handleDeleteModal(data: { event: { id: string } }) {
+    setShowDeleteModal(true);
+    setIdToDelete(Number(data.event.id));
+  }
+
+  function handleDelete() {
+    setAllEvents(
+      allEvents.filter((event) => Number(event.id) !== Number(idToDelete))
+    );
+    setShowDeleteModal(false);
+    setIdToDelete(null);
   }
 
   function handleCloseModal() {
     setShowModal(false);
-    setNewEvent({
-      title: "",
-      start: "",
-      allDay: false,
-      id: 0,
-    });
+    form.reset();
+    setShowDeleteModal(false);
+    setIdToDelete(null);
   }
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setNewEvent({
-      ...newEvent,
-      title: e.target.value,
-    });
-  };
-
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    const newEvent: Event = {
+      title: data.title,
+      start: data.eventDateTime,
+      allDay: false,
+      id: new Date().getTime(),
+      patientName: data.patientName,
+      description: data.description,
+    };
     setAllEvents([...allEvents, newEvent]);
     handleCloseModal();
+    toast({
+      title: "Event added",
+      description: "Your event has been successfully added to the calendar.",
+    });
   }
 
   return (
     <>
-      {/* FullCalendar component setup */}
       <FullCalendar
         plugins={[
           dayGridPlugin,
@@ -110,30 +155,128 @@ export default function Scheduler() {
         dateClick={handleDateClick}
         drop={(data) => addEvent(data)}
         showNonCurrentDates={false}
+        eventClick={(data) => {
+          data.jsEvent.preventDefault();
+          handleDeleteModal(data);
+        }}
       />
 
       <Dialog open={showModal} onOpenChange={setShowModal}>
-        <DialogTrigger asChild>{/* <Button>sdajkd</Button> */}</DialogTrigger>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle>Add New Event</DialogTitle>
+            <DialogTitle>Add Event</DialogTitle>
             <DialogDescription>
               Please enter the event details below.
             </DialogDescription>
           </DialogHeader>
-          <form onSubmit={handleSubmit}>
-            <Input
-              type="text"
-              placeholder="Event Title"
-              value={newEvent.title}
-              onChange={handleChange}
-              required
-            />
-            <Button type="submit">Add Event</Button>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Event Title</FormLabel>
+                    <FormControl>
+                      <Input {...field} required />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="patientName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Patient Name</FormLabel>
+                    <FormControl>
+                      <Input {...field} required />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="eventDateTime"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Date and Time</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant="outline"
+                            className={cn(
+                              "w-full justify-start text-left font-normal",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            <FaCalendar className="mr-2 h-4 w-4" />
+                            {field.value ? (
+                              format(field.value, "PPP HH:mm:ss")
+                            ) : (
+                              <span>Pick a date and time</span>
+                            )}
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          initialFocus
+                        />
+                        <div className="p-3 border-t border-border">
+                          <TimePickerDemo
+                            setDate={field.onChange}
+                            date={field.value}
+                          />
+                        </div>
+                      </PopoverContent>
+                    </Popover>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-end space-x-2">
+                <Button type="submit">Add Event</Button>
+                <Button variant="secondary" onClick={handleCloseModal}>
+                  Cancel
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+
+      <Dialog open={showDeleteModal} onOpenChange={setShowDeleteModal}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Delete Event</DialogTitle>
+            <DialogDescription>
+              Are you sure you want to delete this event?
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex justify-end space-x-2">
+            <Button variant="destructive" onClick={handleDelete}>
+              Delete
+            </Button>
             <Button variant="secondary" onClick={handleCloseModal}>
               Cancel
             </Button>
-          </form>
+          </div>
         </DialogContent>
       </Dialog>
     </>
